4699254fa54a9d1dfa379dbc857e5e6284dfc070
Andrew Cawood <cawooda@gmail.com>
2024-03-22 15:58:01 +1100

    genesis

--- Develop/assets/js/script.js
+++ Develop/assets/js/script.js
@@ -1,15 +1,39 @@
 // Retrieve tasks and nextId from localStorage
 let taskList = JSON.parse(localStorage.getItem("tasks"));
 let nextId = JSON.parse(localStorage.getItem("nextId"));
+let modalForm = $('#modal-form').dialog({
+        autoOpen: false,
+      height: 200,
+      width: 350,
+      modal: true
+      }
+);
+const $dueDate = $('#due-date').datepicker();
+const $addTaskBtn = $('#add-task-btn');
 
 // Todo: create a function to generate a unique task id
+//
 function generateTaskId() {
-
+    return taskList.length() + 1;
+    //read the length of the tasklist and return the next task number
 }
 
 // Todo: create a function to create a task card
 function createTaskCard(task) {
+    
+    const $taskName = $("<h2></h2>");
+    $taskName.text(task.name);
 
+    const $taskDueDate = $("<p></p>");
+    $taskDueDate.text(task.dueDate);
+
+    const $taskDescription = $("<p></p>");
+    $taskDueDate.text(task.description);
+
+
+    const $taskCard = $("<div></div>")
+    $taskCard.append($taskCard)
+    return $taskCard;
 }
 
 // Todo: create a function to render the task list and make cards draggable
@@ -19,6 +43,8 @@ function renderTaskList() {
 
 // Todo: create a function to handle adding a new task
 function handleAddTask(event){
+    //create a task from the information given
+    // task list will need to have a new task added to it.
 
 }
 
@@ -34,5 +60,7 @@ function handleDrop(event, ui) {
 
 // Todo: when the page loads, render the task list, add event listeners, make lanes droppable, and make the due date field a date picker
 $(document).ready(function () {
-
+    addTaskBtn.on('click',function (){
+        modalForm.dialog('open');
+    })
 });
