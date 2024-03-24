94a213bbf0f7e5fdd82fd594325cb9a7bc397e9d
Andrew Cawood <cawooda@gmail.com>
2024-03-24 20:26:48 +1100

    dragable but cant land anywhere

--- Develop/assets/js/script.js
+++ Develop/assets/js/script.js
@@ -6,12 +6,71 @@ const DONE = "done";
 
 let taskList = JSON.parse(localStorage.getItem("tasks"));
 if (!taskList) taskList = [];
+console.log(taskList);
 
-let nextId = JSON.parse(localStorage.getItem("nextId"));
+let nextId = 0;
 
-let $todo = $('#todo-cards');
-let $inProgress = $('#in-progress-cards');
-let $done = $('#done-cards');
+if (localStorage.getItem("nextId")) {
+    nextId = JSON.parse(localStorage.getItem("nextId"));
+} else {
+    let nextId = 1;
+    localStorage.setItem("nextId",nextId);
+}
+
+
+let $todo = $('#todo-cards')
+$todo.droppable({
+    accept: "card",
+    classes: {
+      "ui-droppable-active": "ui-state-active",
+      "ui-droppable-hover": "ui-state-hover"
+    },
+    drop: function( event, ui ) {
+      $( this )
+        .addClass( "ui-state-highlight" )
+        .find( "p" )
+          .html( "Dropped!" );
+    }
+  });
+let $inProgress = $('#in-progress-cards')
+$inProgress.droppable({
+    accept: "card",
+    classes: {
+      "ui-droppable-active": "ui-state-active",
+      "ui-droppable-hover": "ui-state-hover"
+    },
+    drop: function( event, ui ) {
+      $( this )
+        .addClass( "ui-state-highlight" )
+        .find( "p" )
+          .html( "Dropped!" );
+    }
+  });
+let $done = $('#done-cards')
+$done.droppable({
+    accept: "card",
+    classes: {
+      "ui-droppable-active": "ui-state-active",
+      "ui-droppable-hover": "ui-state-hover"
+    },
+    drop: function( event, ui ) {
+      $( this )
+        .addClass( "ui-state-highlight" )
+        .data()
+    }
+  });
+
+$todo.sortable({
+    revert: true
+  });
+
+$inProgress.sortable({
+    revert: true
+  });  
+
+$done.sortable({
+    revert: true
+  });  
 
 let $modalForm = $('#modal-form');
 let $taskName = $('#modal-form #task-name');
@@ -33,44 +92,81 @@ const $addTaskBtn = $('#add-task-btn');
 // Todo: create a function to generate a unique task id
 //
 function generateTaskId() {
-    if (nextId) {
-        localStorage.setItem("nextId",taskList.length() + 1)
-        return nextId;} else return 1;
+    nextId ++;
+    localStorage.setItem("nextId",nextId);
+    return nextId - 1;
     //read the length of the tasklist and return the next task number
 }
 
 // Todo: create a function to create a task card
 function createTaskCard(task) {
-    const $taskCard = $("<div></div>")
+    //take the task and return a jquery 
+    $taskCard = $(`<div id="card-${task.taskId}" data-status="${task.taskStatus}" data-id=${task.taskId}></div>`);
+    $taskCard.draggable({appendTo: "body", connectToSortable: "#todo-cards #in-progress-cards", opacity: 0.35});
 
-    const $taskName = $("<h2></h2>");
-    $taskName.text(task.name);
-    $taskCard.append($taskName);
-
-    const $taskDescription = $("<p></p>");
-    $taskDescription.text(task.description);
-    $taskCard.append($taskDescription);
-
-    const $taskDueDate = $("<p></p>");
-    $taskDueDate.text(task.dueDate);
-    $taskCard.append($taskDueDate);
-
-    const $taskStatus = $("<p></p>");
-    $taskStatus.text(task.status);
-    $taskCard.append($taskStatus);
     
+      
+    $taskCard.addClass('card');
+
+    if (task.taskStatus == TODO) {
+        $taskCard.addClass('bg-primary')
+    } else if (task.taskStatus == IN_PROGRESS) {
+        $taskCard.addClass('bg-success');
+    } else $taskCard.addClass('bg-dark-subtle');
+    
+    const $name = $(`<h4>${task.taskName}</h4>`)
+    $name.addClass('card-title p-3 bg-primary-subtle');
+    
+    const $description = $(`<p>${task.taskDescription}</p>`);
+    $description.addClass('card-text')
+    
+    const $dueDate = $(`<p>${task.taskDate}</p>`)
+    $dueDate.addClass('card-footer fs-6')
+
+    const $status = $(`<p>${task.taskStatus}</p>`)
+    $status.addClass('fs-6')
+
+    const $buttons = $('<div></div>');
+    $buttons.addClass('container row');
+    
+    const $delete = $(`<button data-purpose="delete" type="button">X</button>`)
+        $delete.addClass('btn btn-sm m-3 col btn-danger')
+        $delete.data("purpose","delete");
+    const $done = $(`<button data-purpose="done" type="button">Done</button>`)
+        $done.addClass('btn btn-sm m-3 col btn-success')
+    $buttons.append($delete);
+    $buttons.append($done); 
+
+    $taskCard.append($name);
+    $taskCard.append($status);
+    $taskCard.append($description);
+    $taskCard.append($buttons);
+    $taskCard.append($dueDate);
+
+    
+    $taskCard.on('click',function (event){
+        console.log("button clicked", $(event.target).data("purpose"));
+    })
+    
+    
+
     return $taskCard;
+    //return $taskCard;
 }
 
 // Todo: create a function to render the task list and make cards draggable
 function renderTaskList() {
+    
     for (task of taskList) {
-        console.log(createTaskCard(task))
-        if (task.status == TODO) {
+        
+        if (task.taskStatus == TODO) {
+            createTaskCard(task);
             $todo.append(createTaskCard(task));
-        } else if (task.status == IN_PROGRESS) {
+        } else if (task.taskStatus == IN_PROGRESS) {
+            createTaskCard(task);
             $inProgress.append(createTaskCard(task));
-        } else {
+        } else if (task.taskStatus == DONE) {
+            createTaskCard(task);
             $done.append(createTaskCard(task));
         }
     }
@@ -85,13 +181,17 @@ function handleAddTask(event){
             return false; //returns false of inputs not right. can be relpaced by a flash or something.
         }
 
+        let id = generateTaskId();
+
         let taskAdd = {
             taskName:$taskName.val(),
             taskDescription: $taskDescription.val(),
             taskDate:$dueDate.val(),
             taskStatus: TODO,
-            taskId:function(){return generateTaskId()}
+            taskId: id,
         }
+
+        console.log("task Add id: ",taskAdd.taskId);
 
         $taskName.val('');
         $taskDescription.val('');
